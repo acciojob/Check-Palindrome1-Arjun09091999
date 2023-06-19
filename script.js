953903b52a4341d1a0771b4c532dee8e51808580
Arjun singh gurjar <115486632+Arjun09091999@users.noreply.github.com>
2023-06-19 13:56:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
 // complete the given function
 
 function palindrome(str){
+	const formattedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
 
- var formattedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  // Reverse the formatted string
+  const reversedStr = formattedStr.split('').reverse().join('');
 
-  // Compare characters from both ends of the string
-  for (var i = 0, j = formattedStr.length - 1; i < j; i++, j--) {
-    if (formattedStr[i] !== formattedStr[j]) {
-      return false; // Characters don't match, not a palindrome
-    }
-  }
+  // Check if the reversed string is equal to the formatted string
+  return formattedStr === reversedStr;
 
-  return true; // All characters match, it's a palindrome
 }
-
-// Example usage
-console.log(isPalindrome("racecar")); // true
-console.log(isPalindrome("RaceCar")); // true
-console.log(isPalindrome("raceCAR")); // true
-console.log(isPalindrome("hello")); // false
 module.exports = palindrome
